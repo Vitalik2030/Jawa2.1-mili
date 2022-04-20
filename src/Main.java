3d179436f3932ebada152b217890b20172d570a5
Костенко Виталий <diable2011@mail.ru>
2022-04-20 14:30:01 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 2000;
+        int cashback = 20;
+        int mili = ticket / cashback;
+        System.out.println("Мили" + mili);
+    }
+}
